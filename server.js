c091c05e0274f28824a7fc4a701b97eeb895708a
123aadina <adina.hamida@gmail.com>
2020-09-21 13:16:15 +0200

    server is running

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 /** EXTERNAL DEPENDENCIES */
 const express = require("express");
 
-
+/** INIT */
 const api = express();
+
+
+/** START SERVER*/
+const server = api.listen(3000, () => console.log("server is running"));
