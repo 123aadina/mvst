f715a4c6a0bb3a0dcfd7078387c40af23763ca58
123aadina <adina.hamida@gmail.com>
2020-09-23 11:24:11 +0200

    i made a route, that take token from the header, if there no token it will give error

--- server.js
+++
@@ -1,9 +0,0 @@
-/** EXTERNAL DEPENDENCIES */
-const express = require("express");
-
-/** INIT */
-const api = express();
-
-
-/** START SERVER*/
-const server = api.listen(3000, () => console.log("server is running"));
